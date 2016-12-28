60d0cc1efd8501b2219bfa68f99dfcef041aa729
Timo Klock <timo@Timos-MacBook-Pro.fritz.box>
2016-12-28 17:15:38 +0100

    Restructured things.

---
+++ calculation_utilities/general.py
@@ -0,0 +1,17 @@
+#coding: utf8
+import numpy as np
+
+def relative_error(u1, u2):
+    """ Doc"""
+    assert(u1.shape == u2.shape)
+    return np.linalg.norm(u1 - u2) / np.linalg.norm(u2)
+
+def symmetric_support_difference(support1, support2):
+    """ Adds indices that belong to either the support of u_calc but not the
+    support of u_real, or vice versa to the support of u_real but not the
+    support of u_calc to a list.
+
+    Returns the list of non-overlapping indices as well as the length of
+    non-overlapping nodes. """
+    return len(np.setdiff1d(support1, support2)) + \
+        len(np.setdiff1d(support2, support1))
--- problem_factory/synthetic_random_data.py
+++ problem_factory/synthetic_random_data.py
@@ -2,11 +2,13 @@
 import numpy as np
 
 
-def create_specific_problem_data(n_measurements, n_features, sparsity_level,
-                                 smallest_signal, largest_signal=None,
-                                 noise_type_signal="linf_bounded", noise_lev_signal=0,
-                                 noise_type_measurements="gaussian", noise_lev_measurements=0,
-                                 A=None, random_seed=None, random_state=None):
+def create_unmixing_problem(n_measurements, n_features, sparsity_level,
+                            smallest_signal, largest_signal=None,
+                            noise_type_signal="linf_bounded",
+                            noise_lev_signal=0,
+                            noise_type_measurements="gaussian",
+                            noise_lev_measurements=0,
+                            A=None, random_seed=None, random_state=None):
     """ Method to create a specific data set/example. """
     if random_seed is not None:
         np.random.seed(random_seed)
@@ -19,15 +21,17 @@ def create_specific_problem_data(n_measurements, n_features, sparsity_level,
     y = A.dot(u_real)
     if noise_lev_signal > 0:
         v_real = create_noise(A.shape[1], noise_lev_signal, noise_type_signal,
-            random_state = random_state)
+                              random_state=random_state)
         y = y + A.dot(v_real)
     else:
         v_real = np.zeros(A.shape[1])
     if noise_lev_measurements > 0:
         m_noise = create_noise(A.shape[0], noise_lev_measurements,
-            noise_type_measurements, random_state = random_state)
+                               noise_type_measurements,
+                               random_state=random_state)
         y = y + m_noise
     return A, y, u_real, v_real
+
 
 def create_sampling_matrix(n_measurements, n_features, random_seed=None,
                            random_state=None, scaling="measurements"):
@@ -48,8 +52,10 @@ def create_sampling_matrix(n_measurements, n_features, random_seed=None,
                                                       'normalized_cols'))
     return A
 
+
 def create_sparse_signal(n_features, sparsity_level, smallest_signal,
-                         largest_signal=None, random_seed=None, random_state=None):
+                         largest_signal=None, random_seed=None,
+                         random_state=None):
     if random_seed is not None:
         np.random.seed(random_seed)
     if random_state is not None:
@@ -62,12 +68,14 @@ def create_sparse_signal(n_features, sparsity_level, smallest_signal,
         signal[indices[0:sparsity_level]] = scaling_factor * aux
     else:
         entries = np.random.uniform(low=smallest_signal,
-                                    high=largest_signal, size=(sparsity_level,))
+                                    high=largest_signal,
+                                    size=(sparsity_level,))
         # Ensure minimum entry is equal to c
         entries[np.random.choice(sparsity_level)] = smallest_signal
         sign = np.random.choice([1.0, -1.0], size=(sparsity_level,))
         signal[indices[0:sparsity_level]] = np.multiply(entries, sign)
     return signal
+
 
 def create_noise(length, noise_level, noise_type, random_seed=None,
                  random_state=None):
@@ -76,20 +84,12 @@ def create_noise(length, noise_level, noise_type, random_seed=None,
     if random_state is not None:
         np.random.set_state(random_state)
     if noise_type == "linf_bounded":
-        # aux_noise = np.random.normal(size=length)
-        # scaling_factor_noise = noise_level / np.max(np.abs(aux_noise))
-        # noise = aux_noise * scaling_factor_noise
-        aux_noise = np.random.uniform(low = -1.0, high = 1.0,
-            size=length)
+        aux_noise = np.random.normal(size=length)
         scaling_factor_noise = noise_level / np.max(np.abs(aux_noise))
         noise = aux_noise * scaling_factor_noise
     elif noise_type == "l2_bounded":
-        # aux_noise = np.random.normal(size=length)
-        # scaling_factor_noise = noise_level / np.linalg.norm(aux_noise, ord=2)
-        # noise = aux_noise * scaling_factor_noise
-        aux_noise = np.random.uniform(low = -1.0, high = 1.0,
-            size=length)
-        scaling_factor_noise = noise_level / np.linalg.norm(aux_noise, ord = 2)
+        aux_noise = np.random.normal(size=length)
+        scaling_factor_noise = noise_level / np.linalg.norm(aux_noise, ord=2)
         noise = aux_noise * scaling_factor_noise
     elif noise_type == "gaussian":
         noise = np.random.normal(scale=noise_level ** 2, size=length)
